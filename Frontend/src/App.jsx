e6bfcdfba30a8207f4a69e19afd226ddc94282c6
SarvagyaKumar <sarvagyakrcs@gmail.com>
2024-02-18 21:46:17 +0530

    testing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { msalConfig } from './config/authConfig';
  * We recommend initializing this outside of your root component to ensure it is not re-initialized on re-renders
  */
 const msalInstance = new PublicClientApplication(msalConfig);
-
+// testing
 function App() {
 
     return (
